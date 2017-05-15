b3adc43d5bc44a5c410b3b3fced733940823ca5b
Beth Hansen and Jordan Loop <beth+jordan@>
2017-05-15 15:30:50 -0700

    Add prototype function to check newUser.userStyle to each Campsite.siteStyle and newUser.userSetting to Campsite.siteSetting, and some refactoring.

---
+++ README.md
@@ -0,0 +1,13 @@
+##Specs
+
+* User submits a style, setting, and state for camping.
+  Input: Tent, Coast  Output: Displays camping options that in include camping in a tent at the cost.
+
+* User selects one of the displayed camping options.
+Input: User clicks choice.  Output: Form is displayed.
+
+* User is asked how many nights (dates) and how many people.
+Input: User inputs a number.  Output: Display a price considering how many people and how many nights.
+
+* User is asked to make a reservation or to start over.
+Input: User selects "Make Reservation". Output: User is informed reservation was successful.
--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function Campsite(style, setting, price) {
 };
 
 //  USER CONSTRUCTOR
-function User(style, setting) {
+function User(style, setting, name, email, price, numberOfPeople) {
   this.userName = name;
   this.userEmail = email;
   this.userStyle = style;
@@ -21,26 +21,38 @@ function User(style, setting) {
 };
 
 
-  //  INITIALIZING CAMPING OBJECTS
-var tentMountain = new Campsite(tent, mountain, 5);
-var tentCoast = new Campsite(tent, coast, 5);
-var tentRiver = new Campsite(tent, river, 5);
-var rvMountain = new Campsite(rv, mountain, 5);
-var rvCoast = new Campsite(rv, coast, 5);
-var rvRiver = new Campsite(rv, river, 5);
-var cabinMountain = new Campsite(cabin, mountain, 5);
-var cabinCoast = new Campsite(cabin, coast, 5);
-var cabinRiver = new Campsite(cabin, river, 5);
-
+User.prototype.findCampsite = function(campsiteArray) {
+  for (i = 0; i <= campsiteArray.length; i++) {
+    if (this.userStyle === campsiteArray[i].siteStyle && this.userSetting === campsiteArray[i].siteSetting) {
+      return campsiteArray[i]
+    }
+  }
+}
 
 //  UI LOGIC--------------------------
 $(document).ready(function() {
-  $("*").submit(function(event) {
+  $(".userInput").submit(function(event) {
     event.preventDefault();
-    var selectedStyle = $("#style").val();
-    var selectedSetting = $("#setting").val();
-    newUser = new User(selectdStyle, selectedSetting);
-    console.log(newUser); //-------------------log-----------------------
+
+    //  INITIALIZING CAMPING OBJECTS
+    var tentMountain = new Campsite("tent", "mountain", 5);
+    var tentCoast = new Campsite("tent", "coast", 5);
+    var tentRiver = new Campsite("tent", "river", 5);
+    var rvMountain = new Campsite("rv", "mountain", 5);
+    var rvCoast = new Campsite("rv", "coast", 5);
+    var rvRiver = new Campsite("rv", "river", 5);
+    var cabinMountain = new Campsite("cabin", "mountain", 5);
+    var cabinCoast = new Campsite("cabin", "coast", 5);
+    var cabinRiver = new Campsite("cabin", "river", 5);
+
+    var campsiteArray = [tentMountain, tentCoast, tentRiver, rvMountain, rvCoast, rvRiver, cabinMountain, cabinCoast, cabinRiver];
+
+    var selectedStyle = $("input[name=campStyle]:checked").val();
+    var selectedSetting = $("input[name=settingStyle]:checked").val();
+    newUser = new User(selectedStyle, selectedSetting);
+
+    var test = newUser.findCampsite(campsiteArray);
+    console.log(test);
 
   }); // .submit
 });  // doc.ready
